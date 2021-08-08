fd05bf5361bd79fe5ec111a50976a4b4330b1f9f
Kartik Nair <kartikajitnair@gmail.com>
2021-08-08 18:18:17 +0400

    Note editing using `tiptap` and add header to note page as well

---
+++ src/pages/note/[noteid].tsx
@@ -0,0 +1,170 @@
+/** @jsxImportSource @emotion/react */
+
+import type { Store, Note } from '../../lib/store'
+
+import { css } from '@emotion/react'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+
+import Text from '@tiptap/extension-text'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Placeholder from '@tiptap/extension-placeholder'
+import { useEditor, EditorContent } from '@tiptap/react'
+
+import Header from '../../views/header'
+import useFetch from '../../lib/use-fetch'
+import useDebounce from '../../lib/use-debounce'
+
+function convertTextToHTML(text: string): string {
+  return text
+    .split('\n')
+    .map((line) => {
+      if (line === '\n') return '<p></p>'
+      else return `<p>${line}</p>`
+    })
+    .join('')
+}
+
+function convertHTMLToText(html: string): string {
+  return html
+    .replaceAll('<p></p>', '\n')
+    .replaceAll('<p>', '')
+    .replaceAll('</p>', '\n')
+}
+
+function Editor({
+  title,
+  setTitle,
+  content,
+  setContent,
+}: {
+  title: string
+  setTitle: (newTitle: string) => void
+  content: string
+  setContent: (newContent: string) => void
+}) {
+  const titleEditor = useEditor({
+    content: convertTextToHTML(title),
+    extensions: [
+      Document.extend({ content: 'paragraph' }),
+      Paragraph,
+      Text,
+      Placeholder.configure({
+        placeholder: 'Note title...',
+      }),
+    ],
+    onUpdate: ({ editor: newEditor }) => {
+      setTitle(convertHTMLToText(newEditor.getHTML()))
+    },
+  })
+
+  const contentEditor = useEditor({
+    content: convertTextToHTML(content),
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Placeholder.configure({
+        placeholder: 'Your thoughts...',
+      }),
+    ],
+    onUpdate: ({ editor: newEditor }) => {
+      setContent(convertHTMLToText(newEditor.getHTML()))
+    },
+  })
+
+  return (
+    <>
+      <EditorContent
+        editor={titleEditor}
+        css={css`
+          font-size: 1.25rem;
+          font-weight: 500;
+          color: var(--grey-4);
+          margin-bottom: 2.5rem;
+        `}
+      />
+      <EditorContent editor={contentEditor} />
+    </>
+  )
+}
+
+export default function NoteEditor() {
+  const router = useRouter()
+  const id = router.query.noteid
+
+  const { data: store, error, revalidate } = useFetch<Store>('/api/store')
+  const [note, setNote] = useState<Note>({
+    id: '',
+    content: '',
+    title: '',
+    lastModified: 0,
+  })
+  const [dirid, setDirid] = useState<string>('')
+
+  const [title, setTitle] = useState<string | null>(null)
+  const [content, setContent] = useState<string | null>(null)
+
+  const debouncedTitle = useDebounce(title, 600)
+  const debouncedContent = useDebounce(content, 600)
+
+  useEffect(() => {
+    if (!store) return
+
+    let foundNote = store.notes.find((n) => n.id === id)
+
+    if (!foundNote) {
+      for (let dir of store.directories) {
+        foundNote = dir.notes.find((n) => n.id === id)
+        if (foundNote) {
+          setDirid(dir.id)
+          break
+        }
+      }
+    }
+
+    if (foundNote) {
+      setNote(foundNote)
+      // this only happens when we haven't initialized content/title yet
+      if (content === null) setContent(foundNote.content)
+      if (title === null) setTitle(foundNote.title)
+    }
+  }, [store])
+
+  useEffect(() => {
+    if (
+      debouncedContent !== null &&
+      debouncedTitle !== null &&
+      (note.content !== debouncedContent || note.title !== debouncedTitle)
+    ) {
+      fetch(`/api/new/note?id=${id}${dirid ? `&dir=${dirid}` : ''}`, {
+        method: 'POST',
+        body: debouncedTitle + '\n' + debouncedContent,
+      }).then(() => {
+        console.log('auto saved')
+        revalidate()
+      })
+    }
+  }, [debouncedTitle, debouncedContent])
+
+  if (error) {
+    return <pre>{JSON.stringify(error)}</pre>
+  } else if (!store) {
+    return <p>Loading...</p>
+  } else if (content === null || title === null) {
+    return <p>404, a note with that id was not found</p>
+  }
+
+  return (
+    <>
+      <Header revalidate={revalidate} />
+      <Editor
+        title={title}
+        setTitle={setTitle}
+        content={content}
+        setContent={setContent}
+      />
+    </>
+  )
+}
